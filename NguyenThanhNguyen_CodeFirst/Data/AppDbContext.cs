33326e0aad78347c16f5f8752f12bb1602e20436
NguyenThanhNguyen1535 <231448077+NguyenThanhNguyen1535@users.noreply.github.com>
2026-04-08 20:27:42 +0700

    Add DbContext

---
+++ NguyenThanhNguyen_CodeFirst/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NguyenThanhNguyen_CodeFirst.Models;
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Supplier> Suppliers { get; set; }
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<OrderDetail> OrderDetails { get; set; }
+}
--- NguyenThanhNguyen_CodeFirst/obj/Debug/net10.0/NguyenThanhNguyen_CodeFirst.AssemblyInfo.cs
+++ NguyenThanhNguyen_CodeFirst/obj/Debug/net10.0/NguyenThanhNguyen_CodeFirst.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NguyenThanhNguyen_CodeFirst")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+160459d8b132702728365dff5de90ad313dec2ae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1620881f67f3d00e049886bb1a127bc529123fc0")]
 [assembly: System.Reflection.AssemblyProductAttribute("NguyenThanhNguyen_CodeFirst")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NguyenThanhNguyen_CodeFirst")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
